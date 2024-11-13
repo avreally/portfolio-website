c8990fddff1c6d333c63f6dc9f535d322574947f
Valeria <waleria.shadrina@gmail.com>
2024-11-13 16:29:17 +0100

    small layout fixes

--- src/components/projectInfo/index.tsx
+++ src/components/projectInfo/index.tsx
@@ -2,54 +2,54 @@ import "./styles.css";
 import { ProjectType } from "../../../types";
 
 type ProjectInfoProps = {
-	project: ProjectType;
+  project: ProjectType;
 };
 
 const ProjectInfo = ({ project }: ProjectInfoProps) => {
-	return (
-		<div className="wrapper">
-			<div className="projectInfo">
-				<div className="projectHeader">
-					<h2 className="projectName">{project.name}</h2>
-				</div>
-				<p
-					className="projectAbout"
-					dangerouslySetInnerHTML={{ __html: project.about }}
-				/>
-				<ul className="stack">
-					{project.stack.map((element) => (
-						<li key={element} className="stackElement">
-							{element}
-						</li>
-					))}
-				</ul>
-			</div>
-			{project.websiteUrl || project.githubUrl ? (
-				<div className="projectUrlsWrapper">
-					{project.websiteUrl ? (
-						<a
-							className="projectUrl"
-							target="_blank"
-							rel="noopener noreferrer"
-							href={project.websiteUrl}
-						>
-							[ website ]
-						</a>
-					) : null}
-					{project.githubUrl ? (
-						<a
-							className="projectUrl"
-							target="_blank"
-							rel="noopener noreferrer"
-							href={project.githubUrl}
-						>
-							[ github ]
-						</a>
-					) : null}
-				</div>
-			) : null}
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <div className="projectInfo">
+        <div className="projectHeader">
+          <h2 className="projectName">{project.name}</h2>
+        </div>
+        <p
+          className="projectAbout"
+          dangerouslySetInnerHTML={{ __html: project.about }}
+        />
+        <ul className="stack">
+          {project.stack.map((element) => (
+            <li key={element} className="stackElement">
+              {element}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {project.websiteUrl || project.githubUrl ? (
+        <div className="projectUrlsWrapper">
+          {project.websiteUrl ? (
+            <a
+              className="projectUrl"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={project.websiteUrl}
+            >
+              [ website ]
+            </a>
+          ) : null}
+          {project.githubUrl ? (
+            <a
+              className="projectUrl"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={project.githubUrl}
+            >
+              [ github ]
+            </a>
+          ) : null}
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default ProjectInfo;
