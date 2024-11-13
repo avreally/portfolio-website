a2a4f896118b780dae4999bb62795e7e9b1d2e07
Valeria <waleria.shadrina@gmail.com>
2024-11-13 16:01:44 +0100

    remove icons from navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,33 +1,28 @@
 import "./styles.css";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
-import React from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="headline">contact me:</div>
-      <div className="nav-links">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.linkedin.com/in/valeriashadrina/?locale=en_US"
-        >
-          <FaLinkedin size={35} />
-        </a>
-        <a href="mailto:valeriia.shadrina@hyperisland.se">
-          <IoMail size={35} />
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/avreally"
-        >
-          <FaGithub size={35} />
-        </a>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar">
+			<div className="headline">get in touch:</div>
+			<div className="nav-links">
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.linkedin.com/in/valeriashadrina/?locale=en_US"
+				>
+					[ linkedin ]
+				</a>
+				<a href="mailto:valeriia.shadrina@hyperisland.se">[ email ]</a>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/avreally"
+				>
+					[ github ]
+				</a>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
