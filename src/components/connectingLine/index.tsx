4770aea0c61daa8856ba0f54abd893093aa7583f
Valeria <waleria.shadrina@gmail.com>
2024-11-18 16:18:13 +0100

    add role and title to svgs

--- src/components/connectingLine/index.tsx
+++ src/components/connectingLine/index.tsx
@@ -11,6 +11,7 @@ const connectingLine = ({
 }: ConnectingLineProps) => {
   const lineVariants = [
     <svg
+      role="img"
       className="line small"
       width="563"
       height="105"
@@ -19,6 +20,7 @@ const connectingLine = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <title>An image of connecting line with moving bright green dots</title>
       <path id="path0" className="linePath" d="M4 0V67.9141H74V105" />
       <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
         <animateMotion dur="8s" repeatCount="indefinite">
@@ -37,6 +39,7 @@ const connectingLine = ({
       </rect>
     </svg>,
     <svg
+      role="img"
       className="line big"
       width="563"
       height="105"
@@ -45,6 +48,7 @@ const connectingLine = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <title>An image of connecting line with moving bright green dots</title>
       <path
         id="path1"
         className="linePath"
@@ -82,6 +86,7 @@ const connectingLine = ({
       </rect>
     </svg>,
     <svg
+      role="img"
       className="line small"
       width="563"
       height="105"
@@ -90,6 +95,7 @@ const connectingLine = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <title>An image of connecting line with moving bright green dots</title>
       <path id="path2" className="linePath" d="M559 0V67.9141H489V105" />
       <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
         <animateMotion dur="8s" repeatCount="indefinite">
@@ -108,6 +114,7 @@ const connectingLine = ({
       </rect>
     </svg>,
     <svg
+      role="img"
       className="line big"
       width="563"
       height="105"
@@ -116,6 +123,7 @@ const connectingLine = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <title>An image of connecting line with moving bright green dots</title>
       <path
         id="path3"
         className="linePath"
