4a7870c389c5c404b885f130c3ce32a9950ae75a
Valeria <waleria.shadrina@gmail.com>
2024-11-18 13:51:01 +0100

    change connecting lines and add animation

--- src/components/connectingLine/index.tsx
+++ src/components/connectingLine/index.tsx
@@ -11,40 +11,146 @@ const connectingLine = ({
 }: ConnectingLineProps) => {
   const lineVariants = [
     <svg
-      className="line"
+      className="line small"
+      width="563"
+      height="105"
+      viewBox="0 0 563 105"
+      key={0}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path id="path0" className="linePath" d="M4 0V67.9141H74V105" />
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="8s" repeatCount="indefinite">
+          <mpath href="#path0" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="4s" repeatCount="indefinite">
+          <mpath href="#path0" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="6s" repeatCount="indefinite">
+          <mpath href="#path0" />
+        </animateMotion>
+      </rect>
+    </svg>,
+    <svg
+      className="line big"
+      width="563"
+      height="105"
+      viewBox="0 0 563 105"
       key={1}
-      viewBox="0 0 296 118"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path className="linePath" stroke="black" d="M295 1L295 49H1V117" />
+      <path
+        id="path1"
+        className="linePath"
+        d="M559 105V41.7237H4V1.90735e-06"
+      />
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="11s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path1" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="7s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path1" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="9s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path1" />
+        </animateMotion>
+      </rect>
     </svg>,
     <svg
-      className="line"
+      className="line small"
+      width="563"
+      height="105"
+      viewBox="0 0 563 105"
       key={2}
-      viewBox="0 0 296 118"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path className="linePath" d="M1 1L1 49H295V117" stroke="black" />
+      <path id="path2" className="linePath" d="M559 0V67.9141H489V105" />
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="8s" repeatCount="indefinite">
+          <mpath href="#path2" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="4s" repeatCount="indefinite">
+          <mpath href="#path2" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion dur="6s" repeatCount="indefinite">
+          <mpath href="#path2" />
+        </animateMotion>
+      </rect>
     </svg>,
     <svg
-      className="line"
+      className="line big"
+      width="563"
+      height="105"
+      viewBox="0 0 563 105"
       key={3}
-      viewBox="0 0 296 118"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path className="linePath" d="M295 1L295 49H1V117" stroke="black" />
-    </svg>,
-    <svg
-      className="line"
-      key={4}
-      viewBox="0 0 296 118"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path className="linePath" d="M1 1L1 49H295V117" stroke="black" />
+      <path
+        id="path3"
+        className="linePath"
+        d="M4 105V41.7237H559V1.90735e-06"
+      />
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="11s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path3" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="7s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path3" />
+        </animateMotion>
+      </rect>
+      <rect width={3} height={3} x={-1.5} y={-1.5} fill="#07ff32">
+        <animateMotion
+          dur="9s"
+          repeatCount="indefinite"
+          keyPoints="1;0"
+          keyTimes="0;1"
+        >
+          <mpath href="#path3" />
+        </animateMotion>
+      </rect>
     </svg>,
   ];
 
