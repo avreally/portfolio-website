8f9cd83ae745e781ae1b6dcbebcb464d9c82c2d8
Valeria <waleria.shadrina@gmail.com>
2024-11-18 19:58:01 +0100

    font updates, cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AllProjects from "./components/allProjects";
 import AboutMe from "./components/aboutMe";
 import Navbar from "./components/navbar";
-// import ClientsCarousel from "./components/clientsCarousel";
 
 const App = () => {
   const [data, setData] = useState([]);
-  // const [clientsData, setClientsData] = useState([]);
 
   useEffect(() => {
     getProjectData();
-    // getClientsData();
   }, []);
 
   const getProjectData = async () => {
     const allProjects = await fetch("data/allProjects.json");
     setData(await allProjects.json());
   };
-
-  // const getClientsData = async () => {
-  //   const allClients = await fetch("data/allClients.json");
-  //   setClientsData(await allClients.json());
-  // };
 
   return (
     <div className="app">
