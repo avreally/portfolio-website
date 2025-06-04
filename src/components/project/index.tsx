6f0d7495e385f19008bd326b2f8534d8c4475c1d
Valeria <waleria.shadrina@gmail.com>
2025-06-04 21:34:50 +0200

    remove work experience info

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -5,28 +5,28 @@ import { ProjectType } from "../../../types";
 import { CSSProperties } from "react";
 
 type ProjectProps = {
-  project: ProjectType;
-  imageAlign: "start" | "end";
+	project: ProjectType;
+	imageAlign: "start" | "end";
 };
 
 const Project = ({ project, imageAlign }: ProjectProps) => {
-  return (
-    <div
-      className={`project ${imageAlign === "end" ? "projectImageEnd" : ""}`}
-      style={
-        {
-          "--gradient-direction":
-            imageAlign === "end" ? "to bottom left" : "to bottom right",
-        } as CSSProperties
-      }
-    >
-      <div className="bg"></div>
-      {project.imgUrl ? (
-        <ProjectImage imgUrl={project.imgUrl} websiteUrl={project.websiteUrl} />
-      ) : null}
-      <ProjectInfo project={project} />
-    </div>
-  );
+	return (
+		<div
+			className={`project ${imageAlign === "end" ? "projectImageEnd" : ""}`}
+			style={
+				{
+					"--gradient-direction":
+						imageAlign === "end" ? "to bottom left" : "to bottom right",
+				} as CSSProperties
+			}
+		>
+			<div className="bg"></div>
+			{project.imgUrl ? (
+				<ProjectImage imgUrl={project.imgUrl} websiteUrl={project.websiteUrl} />
+			) : null}
+			<ProjectInfo project={project} />
+		</div>
+	);
 };
 
 export default Project;
